d901e1dd44ad356a35de277bce14fc4362370973
fdaniel13s <u20221c628@upc.edu.pe>
2024-09-16 10:10:01 -0500

    chore: initial commmit

---
+++ src/main/java/com/si642/billmanagementsi642projectbackend/BillManagementSi642ProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.si642.billmanagementsi642projectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillManagementSi642ProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillManagementSi642ProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/si642/billmanagementsi642projectbackend/BillManagementSi642ProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.si642.billmanagementsi642projectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillManagementSi642ProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
